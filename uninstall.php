17e858d620932374f97f9442c5d24c8bad79bc0b
uzzal-koirala <uzzalkoirala@gmail.com>
2025-12-16 06:50:31 +0545

    Add uninstall handler

---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Uninstall handler for Nepsus Link Shortener
+ *
+ * Fired when the plugin is deleted from WordPress.
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
+
+// This plugin does not store any data yet.
+// File reserved for future cleanup (options, tables, etc).
