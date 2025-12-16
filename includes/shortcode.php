f7c9a43c87dda69f98423050dfa313c8f6af0839
uzzal-koirala <uzzalkoirala@gmail.com>
2025-12-16 06:49:09 +0545

    Refactor shortcode with security and validation

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Shortcode handler for Nepsus Link Shortener
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
@@ -9,21 +13,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 function nepsus_fast_shortener() {
 
     $short_url = '';
+    $error     = '';
 
-    if ( isset( $_POST['nepsus_long_url'] ) && ! empty( $_POST['nepsus_long_url'] ) ) {
+    // Handle form submission
+    if (
+        isset( $_POST['nepsus_long_url'], $_POST['nepsus_ls_nonce'] ) &&
+        wp_verify_nonce( $_POST['nepsus_ls_nonce'], 'nepsus_ls_nonce_action' )
+    ) {
 
-        // Sanitize URL
-        $long_url = esc_url_raw( $_POST['nepsus_long_url'] );
+        $long_url = esc_url_raw( trim( $_POST['nepsus_long_url'] ) );
 
-        // is.gd API
-        $api = 'https://is.gd/create.php?format=simple&url=' . urlencode( $long_url );
+        if ( ! empty( $long_url ) ) {
 
-        $response = wp_remote_get( $api, array(
-            'timeout' => 10,
-        ));
+            $api_url = 'https://is.gd/create.php?format=simple&url=' . urlencode( $long_url );
 
-        if ( ! is_wp_error( $response ) ) {
-            $short_url = trim( wp_remote_retrieve_body( $response ) );
+            $response = wp_remote_get(
+                $api_url,
+                array(
+                    'timeout' => 10,
+                )
+            );
+
+            if ( is_wp_error( $response ) ) {
+                $error = 'Something went wrong. Please try again.';
+            } else {
+                $short_url = trim( wp_remote_retrieve_body( $response ) );
+            }
+        } else {
+            $error = 'Please enter a valid URL.';
         }
     }
 
@@ -31,9 +48,13 @@ function nepsus_fast_shortener() {
     ?>
 
     <div class="nepsus-short-box">
+
         <h5>URL Shortener</h5>
 
-        <form method="post">
+        <form method="post" class="nepsus-short-form">
+
+            <?php wp_nonce_field( 'nepsus_ls_nonce_action', 'nepsus_ls_nonce' ); ?>
+
             <input
                 id="longUrlInput"
                 type="url"
@@ -45,9 +66,11 @@ function nepsus_fast_shortener() {
             <button type="submit" class="nepsus-short-btn">
                 Make my URL Short
             </button>
+
         </form>
 
         <?php if ( ! empty( $short_url ) ) : ?>
+
             <div class="nepsus-short-result">
 
                 <strong>Your Short URL:</strong><br>
@@ -70,6 +93,13 @@ function nepsus_fast_shortener() {
                 </div>
 
             </div>
+
+        <?php elseif ( ! empty( $error ) ) : ?>
+
+            <div class="nepsus-short-error">
+                <?php echo esc_html( $error ); ?>
+            </div>
+
         <?php endif; ?>
 
     </div>
